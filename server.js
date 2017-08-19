cdd8c3319f4114f0b8abf2e7b8038ec7050cc0b0
nihit97natu <nihit97natu@gmail.com>
2017-08-19 10:52:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/article-one', function(req,res){
     res.send('Article one requested and will be served here');
-})
+});
 
 app.get('/article-two', function(req,res){
     res.send('Article twp requested and will be served here');
-})
+});
 
 app.get('/article-three', function(req,res){
     res.send('Article three requested and will be served here');
-})
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
